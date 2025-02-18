eaaaddb4fce8bff9ca9c30676b667c5fe1b57233
Hüseyin Erdem Anbarkaya <141375401+erdemanb@users.noreply.github.com>
2025-02-18 14:02:42 +0000

    The course and project has been completed

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -38,6 +38,7 @@ public class DataSource {
     return customer;
   }
   public static Account geAccount(int accountId){
+
     String sql = "select * from accounts where id=?";
     Account account = null;
     try(Connection connection = connect();
@@ -56,9 +57,19 @@ public class DataSource {
         }
         return account;
   }
-  public static void main(String[] args) {
-    Customer customer = getCustomer("twest8o@friendfeed.com");
-    Account account = geAccount(customer.getAccountId());
-    System.out.println(account.getBalance());
+  
+  public static void updateAccountBalance(int accountId, double balance){
+    String sql = "update accounts set balance = ? where id =?";
+    try(
+      Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql);
+    ){
+        statement.setDouble(1, balance);
+        statement.setInt(2, accountId);
+        statement.executeUpdate();
+    }catch(SQLException e){
+        e.printStackTrace();
+    }
   }
+
 }
